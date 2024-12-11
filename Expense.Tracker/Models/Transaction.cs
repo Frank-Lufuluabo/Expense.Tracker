35b3521cd575ddff9998c2c2863cba9c54411652
frankkalengay@gmail.com <frankkalengay@gmail.com>
2024-12-11 23:48:57 +0200

    Add Transaction Controller and Views

--- Expense.Tracker/Models/Transaction.cs
+++ Expense.Tracker/Models/Transaction.cs
@@ -9,8 +9,10 @@ namespace Expense.Tracker.Models
         public int TransactionId { get; set; }
 
         //CategoryId
+        [Required(ErrorMessage = "Please select a category.")]
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
+        [Required(ErrorMessage = "Amount should be greater than 0.")]
         public int Amount { get; set; }
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }

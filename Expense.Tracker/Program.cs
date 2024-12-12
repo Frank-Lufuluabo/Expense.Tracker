7939147cb75a6d9755c01b6a029ab79190ffb053
frankkalengay@gmail.com <frankkalengay@gmail.com>
2024-12-12 23:54:12 +0200

    Add Dashboard and Views

--- Expense.Tracker/Program.cs
+++ Expense.Tracker/Program.cs
@@ -37,7 +37,7 @@ namespace Expense.Tracker
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller= Dashboard}/{action=Index}/{id?}");
 
             app.Run();
         }
